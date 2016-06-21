418180051b2b01aab2759214feb0f24f78e19463
msukenik <m45sukenik@gmail.com>
2016-06-22 00:04:35 +0200

    first commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+	public Text ScoreText;
+	public int score;
+
+	// Use this for initialization
+	void Start () {
+		score = 3;
+		ScoreText.text = "Length: " + score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
